cdfff83e338bd55bdb5b73e131595f0acd65f8c7
k.kosteniuk <konstantinkostenyuk@gmail.com>
2023-01-20 11:21:16 +0100

    All commit

--- src/main/java/com/example/gameshop/GameShopApplication.java
+++ src/main/java/com/example/gameshop/GameShopApplication.java
@@ -2,8 +2,10 @@ package com.example.gameshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class GameShopApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/gameshop/controller/GameController.java
+++ src/main/java/com/example/gameshop/controller/GameController.java
@@ -3,26 +3,36 @@ package com.example.gameshop.controller;
 
 import com.example.gameshop.model.Game;
 import com.example.gameshop.service.GameService;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/game")
 public class GameController {
     private final GameService gameServece;
+    private final Map<Integer, Game> cacheGame= new HashMap<>();
+
 
     public GameController(GameService gameServece) {
         this.gameServece = gameServece;
     }
 
     @GetMapping("/{id}")
+    @Cacheable(value = "games", key = "#id")
+
     public Game getGame(@PathVariable int id) {
-        return this.gameServece.getById(id);
+        System.out.println("@Cacheable");
+        return cacheGame.computeIfAbsent(id, ket ->this.gameServece.getById(id));
     }
 
     @GetMapping()
+    @Cacheable(value = "AllGames")
     public List<Game> getAll() {
+        System.out.println("@Cacheable AllGames");
         return this.gameServece.getAll();
     }
 
